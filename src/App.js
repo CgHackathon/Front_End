93cb088f10271b1da2b157a71d49e9010e596d2c
yousefhassan1999 <yousefhassan121999@gmail.com>
2022-03-12 16:08:52 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Acount from "./Component/Acount.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="landing">
+        <div className="overlay"></div>
+        <div className="containerr">
+          <Acount />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
