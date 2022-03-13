760f6b79cf4546dd3a593886bee7f9b32fc0c63c
yousefhassan1999 <yousefhassan121999@gmail.com>
2022-03-13 09:59:52 +0200

    google style done

--- src/Component/Charity.js
+++ src/Component/Charity.js
@@ -5,10 +5,17 @@ import { useState, useEffect, useRef } from 'react'
 function Charity() {
 
   const [email, setEmail] = useState("");
-  const [amount, setAmount] = useState(0);
-
+  const [amount1, setAmount] = useState(0);
+  const getInputValueemail = (event) => {
+    setEmail( event.target.value)
+  };
+  const getInputValueamount1 = (event) => {
+    setAmount( event.target.value)
+  };
   function submitDonation() {
-    axios.post('http://localhost:8080/donation/donate', { email, amount }).then((res) => {
+    console.log(email);
+    console.log(amount1);
+    axios.post('http://localhost:8080/donation/donate', {charityName: email,amount: amount1 }).then((res) => {
       console.log(res);
     });
   }
@@ -19,7 +26,7 @@ function Charity() {
         <Form className="col-md-6 bg-danger text-white bg-opacity-25 mb-2 p-4">
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Name of charity</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" onChange={event => setEmail(Object.assign(email, { "Email": event.target.value }))} />
+            <Form.Control type="email" placeholder="Enter email" onChange={getInputValueemail} />
             <Form.Text className="text-white">
               We'll never share your name with anyone else.
             </Form.Text>
@@ -27,7 +34,7 @@ function Charity() {
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Money</Form.Label>
-            <Form.Control type="text" placeholder="Amount" onChange={event => setAmount(Object.assign(amount, { "Amount": event.target.value }))} />
+            <Form.Control type="text" placeholder="Amount" onChange={getInputValueamount1} />
           </Form.Group>
           <Button variant="danger" type="submit" onClick={submitDonation}>
             Send
