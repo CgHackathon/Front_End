3d863891b0c07acb8e823e935adf95ea1e55cb79
yousefhassan1999 <yousefhassan121999@gmail.com>
2022-03-13 08:45:54 +0200

    homepage

--- src/Component/HomePage.jsx
+++ src/Component/HomePage.jsx
@@ -8,12 +8,39 @@ import "./HomePage.css";
 import axios from "axios";
 
 const HomePage = () => {
-  const initialValue = [
-    { id: 1, from: "ahmed", to: "mohamed", time: "111111" },
-  ];
+  const initialValue = [];
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/user/getInfo`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+      .then((res) => {
+        console.log(res);
+      });
+    GetReports();
+  }, []);
+
+  const selectedrepo = {
+    id: "",
+    userName: "",
+    from: { userName: "" },
+    to: { userName: "" },
+    time: "",
+  };
+  const selectedpageowner = {
+    id: "",
+    userName: "",
+    from: { userName: "" },
+    to: { userName: "" },
+    time: "",
+  };
+
   const [show, setShow] = useState(false);
   const [repo, setShowrepo] = useState(true);
   const [reportsList, setShowrepoList] = useState(initialValue);
+  const [repoShow, setrepoShow] = useState(selectedrepo);
+  const [pageowner, setpageowner] = useState(selectedrepo);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -23,13 +50,17 @@ const HomePage = () => {
 
   const GetReports = () => {
     handleCloserepo();
+    GetReportsaxios();
+  };
+
+  const GetReportsaxios = () => {
     axios
       .get(`http://localhost:8080/doctor/getReport`, {
         headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
       })
       .then((res) => {
         console.log(res);
-        setShowrepoList(res.data)
+        setShowrepoList(res.data);
       });
   };
   const GetMessages = () => {
@@ -38,14 +69,15 @@ const HomePage = () => {
 
   const Tablehandle = (e) => {
     console.log(e);
+    setrepoShow(e);
     handleShow();
   };
   const reportsLists = reportsList.map((Report) => {
     return (
       <tr key={Report.id} onClick={() => Tablehandle(Report)}>
         <th scope="row">{Report.id}</th>
-        <td>{Report.from.email}</td>
-        <td>{Report.to.email}</td>
+        <td>{Report.from.userName}</td>
+        <td>{Report.to.userName}</td>
         <td>{Report.time}</td>
       </tr>
     );
@@ -78,16 +110,6 @@ const HomePage = () => {
       <div className="row">
         <Sidebar></Sidebar>
         <div className="col-md-8 mt-2">
-          {/* <div className="card text-center content bg-danger text-white bg-opacity-50">
-          <h1 className="m-3 pt-3">Current Balance</h1>
-          <div className="card-body">
-            <div className="row">
-              <div className="col-md-20">
-                <h5>FULL Name</h5>
-              </div>
-            </div>
-          </div>
-        </div> */}
           <div className="card text-center content bg-white text-white bg-opacity-25 mt-2">
             {!repo ? (
               <h1 className="pt-3">Reports List</h1>
@@ -115,26 +137,47 @@ const HomePage = () => {
                 <Form>
                   <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>ID</Form.Label>
-                    <Form.Control type="email" placeholder="Enter Amount" />
+                    <Form.Control
+                      type="email"
+                      placeholder={repoShow.id}
+                      disabled
+                    />
                   </Form.Group>
                   <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>From</Form.Label>
-                    <Form.Control type="email" placeholder="Enter Amount" />
+                    <Form.Control
+                      type="email"
+                      placeholder={repoShow.from.userName}
+                      disabled
+                    />
                   </Form.Group>
                   <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>To</Form.Label>
-                    <Form.Control type="email" placeholder="Enter Amount" />
+                    <Form.Control
+                      type="email"
+                      placeholder={repoShow.to.userName}
+                      disabled
+                    />
                   </Form.Group>
                   <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Time</Form.Label>
-                    <Form.Control type="email" placeholder="Enter Amount" />
+                    <Form.Control
+                      type="email"
+                      placeholder={repoShow.time}
+                      disabled
+                    />
                   </Form.Group>
                   <Form.Group
                     className="mb-3"
                     controlId="exampleForm.ControlTextarea1"
                   >
                     <Form.Label>{!repo ? "Report" : "Message"}</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
+                    <Form.Control
+                      as="textarea"
+                      rows={3}
+                      placeholder={repoShow.content}
+                      disabled
+                    />
                   </Form.Group>
                 </Form>
               </Modal.Body>
@@ -147,57 +190,6 @@ const HomePage = () => {
                 </Button>
               </Modal.Footer>
             </Modal>
-            {/* <div className="card-body">
-              <Carousel className="corssal">
-                <div className="card" width="18rem">
-                  <img
-                    src={companyLogo1}
-                    className="card-img-top"
-                    alt="..."
-                  ></img>
-                  <div className="card-body">
-                    <h5 className="card-title text-dark">Card title</h5>
-                    <p className="card-text text-dark">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <Button variant="danger" onClick={handleShow}>
-                      Launch demo modal
-                    </Button>
-
-                    <Modal show={show} onHide={handleClose}>
-                      <Modal.Header closeButton>
-                        <Modal.Title>Money Transfer</Modal.Title>
-                      </Modal.Header>
-                      <Modal.Body>
-                        <Form>
-                          <Form.Group
-                            className="mb-3"
-                            controlId="formBasicEmail"
-                          >
-                            <Form.Label>
-                              Enter Amount of Transfer Money
-                            </Form.Label>
-                            <Form.Control
-                              type="email"
-                              placeholder="Enter Amount"
-                            />
-                          </Form.Group>
-                        </Form>
-                      </Modal.Body>
-                      <Modal.Footer>
-                        <Button variant="secondary" onClick={handleClose}>
-                          Cancle
-                        </Button>
-                        <Button variant="primary" onClick={handleClose}>
-                          Send
-                        </Button>
-                      </Modal.Footer>
-                    </Modal>
-                  </div>
-                </div>
-              </Carousel>
-            </div> */}
           </div>
         </div>
       </div>
